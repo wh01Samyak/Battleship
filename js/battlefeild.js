7d68dec7d0a318933ca6d215d6e5fbd42da3f14f
wh01Samyak <samyakbagra83@gmail.com>
2020-05-19 17:18:07 +0530

    add support for game board

--- js/battlefeild.js
+++ js/battlefeild.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
-	$('#feild').click( function(){
-		$('#feild').find('td').click( function(){
+	$('#opponent-feild').click( function(){
+		$('#opponent-feild').find('td').click( function(){
+			$(this).css("background-color", "red");
 			console.log('You clicked row '+ ($(this).closest('tr').index() +1) +' and column '+($(this).index()+1) );
 		});
 	});
