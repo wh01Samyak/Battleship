32d24d6a2ae02323ff31047794c165eceb28f3d2
wh01Samyak <samyakbagra83@gmail.com>
2020-05-20 03:06:49 +0530

    correct logic for creating feild

--- js/battlefeild.js
+++ js/battlefeild.js
@@ -9,43 +9,44 @@ $(document).ready(function(){
 
 function create_battle_map(){
 	var matrix = [];
-	for(var i=0; i<9; i++) {
+	for(var i=0; i<10; i++) {
 		matrix[i] = [];
-		for(var j=0; j<9; j++) {
+		for(var j=0; j<10; j++) {
 			matrix[i][j] = 0;
 		}
 	}
-	console.log(matrix);
 	for (var i = 4; i > 0; i--) {
-		for (var j = 0; j < 5-i; i++) {
+		for (var j = 0; j < 5-i; j++) {
 			var orientation = Math.round(Math.random()); // 0 means horizontal, 1 means vertical.
 			var x_limit = 10;
 			var y_limit = 10;
 			if (orientation==0){
-				x_limit-=i+1;
+				x_limit=10-i+1;
 			}
 			else{
-				y_limit-=i+1;
+				y_limit=10-i+1;
 			}
 			var flag = true;
 			while(flag) {
 				flag = false;
-				var x_initial = Math.round(Math.random() * x_limit);
-				var y_initial = Math.round(Math.random() * y_limit);
+				var x_initial = Math.floor(Math.random() * x_limit);
+				var y_initial = Math.floor(Math.random() * y_limit);
 				for (var k = 0; k < i; k++) {
 					if (orientation==0){
 						if(matrix[x_initial+k][y_initial]==1){
 							flag=true;
+							continue;
 						}
 					}
 					else{
 						if(matrix[x_initial][y_initial+k]==1){
 							flag=true;
+							continue;
 						}
 					}
 				}
-				console.log("hello ji hello");
 				if(flag == false) {
+					console.log("hello hello");
 					for (var k = 0; k < i; k++) {
 						if (orientation==0){
 							matrix[x_initial+k][y_initial]=1;
@@ -55,7 +56,6 @@ function create_battle_map(){
 						}
 					}
 				}
-				console.log(matrix);
 			}
 		}
 	}
